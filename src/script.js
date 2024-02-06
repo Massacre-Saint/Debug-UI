ad7cb77e1d93e4375d21bd6d949654b915cafaee
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2024-02-06 09:16:28 -0600

    Refactored functions and created main function

--- src/script.js
+++ src/script.js
@@ -1,87 +1,80 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl');
+const main = () => {
+  // Canvas
+  const canvas = document.querySelector('canvas.webgl');
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
 
-// Scene
-const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene();
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-
-scene.add(mesh);
-
-// Sizes
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
-// Camera
-const frustrum = {
-  fov: 75,
-  aspect: sizes.width / sizes.height,
-  near: 0.1,
-  far: 100,
-};
-
-const camera = new THREE.PerspectiveCamera(
-  frustrum.fov,
-  frustrum.aspect,
-  frustrum.near,
-  frustrum.far,
-);
-
-camera.position.z = 3;
-
-scene.add(camera);
-
-// Cursor Controls
-const cursor = {
-  x:0,
-  y:0
-};
-window.addEventListener('mousemove', (event) => {
-  console.log(cursor.x, cursor.y)
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = event.clientY / sizes.height - 0.5; // negate for y axis
-});
-
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-
-// Renderer
-const renderer = new THREE.WebGL1Renderer({ canvas });
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.render(scene, camera);
-
-window.addEventListener('resize', () => {
-  // Update Sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update Camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update Renderer
+  // Renderer
+  const renderer = new THREE.WebGL1Renderer({ canvas });
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  renderer.render(scene, camera);
-});
 
-const tick = () => {
+  // Camera
+  const fov = 75;
+  const aspect = sizes.width / sizes.height;
+  const near = 0.1;
+  const far = 100;
 
-  controls.update();
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(1, 1, 3);
 
-  // Render
-  renderer.render(scene, camera);
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
+  scene.add(camera);
+
+  // Object(s)
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh);
+
+  // Cursor Controls
+  const cursor = {
+    x: 0,
+    y: 0,
+  };
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+
+  // Listeners
+  window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = event.clientY / sizes.height - 0.5; // negate for y axis
+  });
+
+  window.addEventListener('resize', () => {
+    // Update Sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update Camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update Renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.render(scene, camera);
+  });
+
+  const tick = () => {
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+  };
+
+  return tick();
 };
 
-tick();
+main();
